eedf85e7aa9fb6707f80d5be75077d84a678d992
Himanshu <141491720+Himanshu-sh0@users.noreply.github.com>
2025-04-08 23:52:15 +0530

    Update desktop-setup.sh

--- desktop-setup.sh
+++ desktop-setup.sh
@@ -4,23 +4,35 @@ curl -O https://raw.githubusercontent.com/Himanshu-sh0/my_desktop/refs/heads/mai
 curl -O https://raw.githubusercontent.com/Himanshu-sh0/my_desktop/refs/heads/main/restore-default-desktop.sh
 
 chmod +x termux-desktop.sh
-chmod +x create-macos-script.sh
-chmod +x restore-default-desktop.sh
+#!/bin/bash
 
-./termux-desktop.sh
-./create-macos-script.sh
-clear
-echo "➡-------------------------------"
-echo -e "\n✅ Commands added successfully!"
-echo "➡-------------------------------"
-echo "➡ Run \`install-termux-desktop\` to install the desktop."
-echo "➡ Run \`start-termux-desktop\` to start the desktop."
-echo "➡-------------------------------"
-echo "➡ Install Size:- 2.5-GB."
+# Define colors
+GREEN='\033[0;32m'    # Green
+BLUE='\033[0;34m'     # Blue
+CYAN='\033[0;36m'     # Cyan
+
+# Font weights
+BOLD='\033[1m'
+FAINT='\033[2m'
+NORMAL='\033[22m'  
+
+log(){
+    local command=$1
+    local message=$2
+    printf "${CYAN}${FAINT}Run ➡ ${NC}${GREEN}${BOLD}%s${NC}\n ${BLUE}${NORMAL}%s${NC}\n${GREEN}${BOLD}--------------------------------${NC}\n" "$command" "$message"
+}
+size_log(){
+    local size=$1
+    printf "${CYAN}${FAINT}Size :-${NC} ${GREEN}${BOLD}%s${NC}\n${BLUE}${BOLD}--------------------------------${NC}\n\n" "$size"
+}
+
+log "install-termux-desktop" "to install the desktop."
+size_log "2.5-GB"
+log "start-termux-desktop" "to start the desktop."
+echo ""
+log "macos-install" "to intall macos customization files."
+size_log "0.7-GB"
+log "macos-customize" "to customize the desktop"
 echo "➡-------------------------------\n"
-echo "➡ Run \`macos-install\` to install macos customization files."
-echo "➡ Run \`macos-customize\` to customize the desktop."
-echo "➡-------------------------------"
-echo "➡ Total Install Size :- 3.5-GB."
-echo "➡-------------------------------"
 rm -rf desktop-setup.sh
+
